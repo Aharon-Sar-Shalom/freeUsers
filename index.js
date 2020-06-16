bc95f296f4690c0dd4639ef47e0499467b1be5e6
Aharon <ahronsarshalom@gmail.com>
2020-06-16 12:20:27 +0300

    Aharon crud

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.post('/users', async (req, res) => {
   }
 });
 
-//get all users
+//get all users(for manager)
 app.get('/users', async (req, res) => {
   try {
     res.send(await collection.find({}).toArray())
@@ -38,15 +38,48 @@ app.get('/users', async (req, res) => {
   } 
 });
 
-// //Aharon
+// //Aharon-GET himself
 app.get('/users/Aharon', async (req, res) => {
   try {
-    res.send(await collection.findOne({ firstName: +req.params.firstName}))
+    res.send(await collection.findOne({ firstName: 'Aharon'}))
   } catch (e) {
     res.send(e);
   } 
 });
 
+// //Aharon-DELETE himself
+app.delete('/users/Aharon', async (req, res) => {
+  try {
+    res.send(await collection.deleteOne({ firstName: 'Aharon'}))
+    res.status(200).json('ok');
+  } catch (e) {
+    res.send(e);
+  } 
+});
+
+//Aharon POST new user
+app.post('/users/Aharon', async (req, res) => {
+  const {firstName, lastName, age, password} = req.body;
+  try {
+    await collection.insertOne({firstName, lastName, age, password});
+    res.status(200).json('ok');
+  } catch (e) {
+    res.send(e);
+  }
+});
+
+// //Aharon PUT himself
+app.put('/users/Aharon', async (req, res) => {
+  try {
+    const {firstName, lastName, age, password} = req.body;
+    await collection.updateOne(
+      { firstName: 'Aharon' },{ $set: {firstName, lastName, age, password} }
+    );
+    res.status(200).json('ok');
+  } catch (e) {
+    res.send(e);
+  }
+});
 
 
 app.listen(8000, () => {
