4734c71c8cf835e93abffcbc65fa8b25fc1275dd
Aharon <ahronsarshalom@gmail.com>
2020-06-21 11:13:09 +0300

    clean my code

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ let collection = null;
 
 /////////freeUsers mission:
 
-////manager options:
-// DELETE all users(manager option)
+
+// DELETE all users
 app.delete('/users/reset', async (req, res) => {
   try {
     await collection.deleteMany({});
@@ -28,7 +28,7 @@ app.delete('/users/reset', async (req, res) => {
     res.send(e);
   }
 });
-// DELETE one user(manager option)
+// DELETE one user
 app.delete('/users/:id', async (req, res) => {
   try {
     await collection.deleteOne({fullName: req.params.id});
@@ -37,17 +37,17 @@ app.delete('/users/:id', async (req, res) => {
     res.send(e);
   }
 });
-//POST new user(manager option)
+//POST new user
 app.post('/users', async (req, res) => {
-  const {firstName, lastName, fullName, age, password} = req.body;
+  const {firstName, lastName, age, email, password} = req.body;
   try {
-    await collection.insertOne({firstName, lastName, fullName, age, password});
+    await collection.insertOne({firstName, lastName, age, email, password});
     res.status(200).json('ok');
   } catch (e) {
     res.send(e);
   }
 });
-//GET all users(manager option)
+//GET all users
 app.get('/users', async (req, res) => {
   try {
     res.send(await collection.find({}).toArray())
@@ -55,16 +55,6 @@ app.get('/users', async (req, res) => {
     res.send(e);
   } 
 });
-// //GET spceific user(manager option)
-app.get('/users/:id', async (req, res) => {
-  try {
-    res.send(await collection.findOne({ _id: +req.params.id}))
-  } catch (e) {
-    res.send(e);
-  } 
-});
-
-////users options:
 // //GET spceific user
 app.get('/users/:id', async (req, res) => {
   try {
@@ -72,40 +62,6 @@ app.get('/users/:id', async (req, res) => {
   } catch (e) {
     res.send(e);
   } 
-});
-
-// //Aharon-DELETE himself
-app.delete('/users/Aharon', async (req, res) => {
-  try {
-    res.send(await collection.deleteOne({ firstName: 'Aharon'}))
-    res.status(200).json('ok');
-  } catch (e) {
-    res.send(e);
-  } 
-});
-
-//Aharon POST new user
-app.post('/users/Aharon', async (req, res) => {
-  const {firstName, lastName, age, password} = req.body;
-  try {
-    await collection.insertOne({firstName, lastName, age, password});
-    res.status(200).json('ok');
-  } catch (e) {
-    res.send(e);
-  }
-});
-
-// //Aharon PUT himself
-app.put('/users/Aharon', async (req, res) => {
-  try {
-    const {firstName, lastName, age, password} = req.body;
-    await collection.updateOne(
-      { firstName: 'Aharon' },{ $set: {firstName, lastName, age, password} }
-    );
-    res.status(200).json('ok');
-  } catch (e) {
-    res.send(e);
-  }
 });
 
 
